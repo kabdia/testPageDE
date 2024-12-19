33b9f9f4336a7cfc9b828314787e679529e9ed8c
Kabakova <kabdia@mail.ru>
2024-12-20 02:03:39 +0500

    feat: add header

--- src/js/utils/style.js
+++ src/js/utils/style.js
@@ -2,4 +2,5 @@ import '../../style/reset.scss';
 import '../../style/vars.scss';
 import '../../style/global.scss';
 import '../../style/pallete.scss';
+import '../../style/fonts.scss';
 import '../../style/sections/header.scss';
